4b168bdc36e1032eb388d40865fa0e8c9bff219c
Asitha De Alwis <w.dealwis@ucdconnet.ie>
2019-08-05 20:29:42 +0530

    Minor code correction.

--- src/main/java/com/ifs/asdelk/ODateEntityMapper.java
+++ src/main/java/com/ifs/asdelk/ODateEntityMapper.java
@@ -1,31 +1,22 @@
 package com.ifs.asdelk;
 
-import org.apache.olingo.client.api.domain.ClientComplexValue;
 import org.apache.olingo.client.api.domain.ClientEntity;
 import org.apache.olingo.client.api.domain.ClientPrimitiveValue;
 import org.apache.olingo.client.api.domain.ClientProperty;
 import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeKind;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ODateEntityMapper {
 
     public static <E> E readFromClient(ClientEntity clientEntity, Class<E> entityClass) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Constructor<E> constructor = entityClass.getConstructor(new Class[]{entityClass});
-        E object = constructor.newInstance(new Object[]{});
+        E object = entityClass.newInstance();
 
         for(ClientProperty cp : clientEntity.getProperties()){
             String fieldName = cp.getName();
             ClientPrimitiveValue primitiveValue = cp.getPrimitiveValue();
-            String prepending;
-            if(primitiveValue.getTypeKind() == EdmPrimitiveTypeKind.Boolean){
-                prepending = "is";
-            }else{
-                prepending = "set";
-            }
-            String setterMethodName = prepending.concat(fieldName);
+            String setterMethodName = "set".concat(fieldName);
 
             if(primitiveValue.getTypeKind() == EdmPrimitiveTypeKind.Boolean){
                 Class[] methodParams = new Class[1];
