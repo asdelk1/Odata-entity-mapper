fc5704273d4d0aa49f35118c3490513208dbf2c1
Asitha De Alwis <w.dealwis@ucdconnet.ie>
2019-08-06 08:53:13 +0530

    Minor code correction.

--- src/main/java/com/ifs/asdelk/ODateEntityMapper.java
+++ src/main/java/com/ifs/asdelk/ODateEntityMapper.java
@@ -3,14 +3,16 @@ package com.ifs.asdelk;
 import org.apache.olingo.client.api.domain.ClientEntity;
 import org.apache.olingo.client.api.domain.ClientPrimitiveValue;
 import org.apache.olingo.client.api.domain.ClientProperty;
+import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeException;
 import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeKind;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.LocalDateTime;
 
 public class ODateEntityMapper {
 
-    public static <E> E readFromClient(ClientEntity clientEntity, Class<E> entityClass) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public static <E> E readFromClient(ClientEntity clientEntity, Class<E> entityClass) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, EdmPrimitiveTypeException {
         E object = entityClass.newInstance();
 
         for(ClientProperty cp : clientEntity.getProperties()){
@@ -19,19 +21,23 @@ public class ODateEntityMapper {
             String setterMethodName = "set".concat(fieldName);
 
             if(primitiveValue.getTypeKind() == EdmPrimitiveTypeKind.Boolean){
-                Class[] methodParams = new Class[1];
-                methodParams[0] = Boolean.class;
-                Method setterMethod = entityClass.getDeclaredMethod(setterMethodName, methodParams);
-                setterMethod.invoke(object, new Boolean(cp.getPrimitiveValue().toString()));
+                ODateEntityMapper.methodInvoker(entityClass, setterMethodName, Boolean.class, cp.getPrimitiveValue().toCastValue(Boolean.class), object);
             }else if(primitiveValue.getTypeKind() == EdmPrimitiveTypeKind.String){
-                Class[] methodParams = new Class[1];
-                methodParams[0] = Boolean.class;
-                Method setterMethod = entityClass.getDeclaredMethod(setterMethodName, methodParams);
-                setterMethod.invoke(object, cp.getPrimitiveValue().toString());
+                ODateEntityMapper.methodInvoker(entityClass, setterMethodName, Boolean.class, cp.getPrimitiveValue().toCastValue(String.class), object);
+            } else if (primitiveValue.getTypeKind() == EdmPrimitiveTypeKind.DateTimeOffset) {
+                ODateEntityMapper.methodInvoker(entityClass, setterMethodName, Boolean.class, cp.getPrimitiveValue().toCastValue(LocalDateTime.class), object);
+
             }
 
         }
         return object;
+    }
+
+    private static <T> void methodInvoker(Class objectClass, String methodName, Class paramType, Object param, T object) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class[] methodParams = new Class[1];
+        methodParams[0] = paramType;
+        Method setterMethod = objectClass.getDeclaredMethod(methodName, methodParams);
+        setterMethod.invoke(object, param);
     }
 
 }
